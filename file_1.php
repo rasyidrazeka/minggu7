a35c204d3a1ba1633740db2d830c92c3dc5c3f42
rasyidrazeka <rasyidrazeka@gmail.com>
2022-04-04 18:15:34 +0700

    Jobsheet 7 Rasyid

---
+++ file_1.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+            $myFile="testFile.txt";
+            $fh=fopen($myFile, 'r');
+            $theData=fgets($fh);
+            fclose($fh);
+            echo $theData;
+        ?>
+    </body>
+</html>
